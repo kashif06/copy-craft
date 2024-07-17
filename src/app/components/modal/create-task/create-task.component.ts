ac97816978141712702a66ba092fbd027c1cafbb
kashif-shaikh <kashif.shaikh@dreambridge.global>
2024-07-17 20:07:17 +0530

    Working on adding task modal

--- src/app/components/modal/create-task/create-task.component.ts
+++ src/app/components/modal/create-task/create-task.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import type { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-task',
@@ -13,7 +14,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateTaskComponent  implements OnInit {
 
+  inputModel = '';
+  title = '';
+  description = '';
+
+  @ViewChild('ionInputEl', { static: true }) ionInputEl!: IonInput;
+  @ViewChild('ionInputEl2', { static: true }) ionInputEl2!: IonInput;
+
   taskForm!: FormGroup;
+  isSaveDisabled: boolean = true;
+  isDetailsVisible: boolean = false;
+  starIcon: string = "assets/icon/star-outline.svg";
+  starIconToggle: boolean = false;
 
   constructor(private modalCtrl: ModalController, private fb: FormBuilder) { }
 
@@ -23,21 +35,23 @@ export class CreateTaskComponent  implements OnInit {
       description: [''],
       dueDate: [null],
     });
-    setTimeout(() => {
-      this.focusAndOpenKeyword();
-    }, 300);
   }
 
-  focusAndOpenKeyword() {
-    // let el:any = document.querySelector(".block > .swiper-slide-active");
-    // let elInput = el.querySelector('.native-input') as HTMLElement || el.querySelector('.native-textarea') as HTMLElement;
-
-    // if (elInput) {
-    //   elInput.setAttribute('autofocus', 'autofocus');
-    //   elInput.focus();
-    //   elInput.click();
-    // }
+  onInputTitle(ev:any) {
+    const value = ev.target!.value;
+    this.isSaveDisabled = value == "";
+    // Removes non alphanumeric characters
+    const filteredValue = value.replace(/[^a-zA-Z0-9]+/g, '');
+    this.ionInputEl.value = this.title = filteredValue;
   }
+  
+  onInputDescription(ev:any) {
+    const value = ev.target!.value;
+    // Removes non alphanumeric characters
+    const filteredValue = value.replace(/[^a-zA-Z0-9]+/g, '');
+    this.ionInputEl2.value = this.description = filteredValue;
+  }
+
 
   dismissModal() {
     this.modalCtrl.dismiss();
@@ -47,6 +61,16 @@ export class CreateTaskComponent  implements OnInit {
     // Implement logic to handle task submission (e.g., save to database, close modal)
     console.log('Task Submitted:', this.taskForm.value); // Example log
     this.dismissModal();
+  }
+
+  toggleDetails() {
+    this.isDetailsVisible = !this.isDetailsVisible;
+  }
+
+  toggleStar() {
+    this.starIconToggle = !this.starIconToggle;
+
+    this.starIcon = (this.starIconToggle == true) ? "assets/icon/star-filled.svg": "assets/icon/star-outline.svg";
   }
 
 }
